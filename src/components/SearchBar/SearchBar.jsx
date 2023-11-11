884bb7a4f7aa0cc65169047206125beae61146b5
MariiaSvitsa <mariia0198@gmail.com>
2023-11-11 18:30:09 +0200

    +

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,39 +1,18 @@
-import { Component } from 'react';
-import css from './SearchBar.module.css';
 
-export class SearchBar extends Component {
-  state = {
-    inputValue: '',
-  };
+import React from 'react';
+import SearchForm from '../SearchForm/SearchForm';
+import PropTypes from 'prop-types';
 
-  handleChangeValue = ({ target }) => {
-    this.setState({ inputValue: target.value });
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state.inputValue);
-    this.setState({ inputValue: '' });
-  };
-  render() {
-    return (
-      <header className={css.SearchBar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="css" className={css.SearchForm_button}>
-            <span className={css.button_label}>Search</span>
-          </button>
+import styles from './SearchBar.module.css';
 
-          <input
-            className={CSS.SearchForm_input}
-            type="text"
-            autoComplete="off"
-            value={this.state.value}
-            onChange={this.handleChangeValue}
-            autoFocus
-            placeholder="Search images and photos"
-            name="searcher"
-          />
-        </form>
-      </header>
-    );
-  }
+export default function SearchBar({ onSubmit }) {
+  return (
+    <header className={styles.SearchBar}>
+      <SearchForm onSubmit={onSubmit} />
+    </header>
+  );
 }
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
